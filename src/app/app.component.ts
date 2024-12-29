7f2b4da8b227db0b335ee4b14bff1b6e0a72ec4b
Hamza hanfi <hanfihamza20@gmail.com>
2024-12-30 00:51:37 +0100

    feat/c12-l87

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {}
+export class AppComponent {
+  public form = new FormGroup({
+    name: new FormControl(""),
+    email: new FormControl(""),
+    password: new FormControl(""),
+  });
+
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
+}
